98bd6cb371def83872ebd5d082b25f8e8753afe4
Edison Chen <jeepchenup@foxmail.com>
2020-08-28 14:47:24 +0800

    创建Arthas-In-Action项目

---
+++ Spring-Boot-In-Action/Spring-Boot-Arthas-Inaction/Arthas-In-Action.md
@@ -0,0 +1,4 @@
+# Arthas In Action
+为了演示这个功能，需要额外的下载arthas-tunnel-server-3.3.9.jar
+
+这个jar是用来展示UI界面
---
+++ Spring-Boot-In-Action/Spring-Boot-Arthas-Inaction/src/main/java/com/inaction/arthas/Application.java
@@ -0,0 +1,16 @@
+package com.inaction.arthas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Spring Boot 整合 Arthas
+ *
+ * @author pc.chen
+ */
+@SpringBootApplication
+public class Application {
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+}
